7af17e6d4c7e7549786c29724c324acc48f4f1a5
plathrive <muhrasyid21048@gmail.com>
2026-03-05 10:49:17 +0700

    B: adding delta time fps to the screen

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,12 @@ def main():
 
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
+
+
 
 
 if __name__ == "__main__":
