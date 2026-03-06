28b7d6fe133071c4cb5619ea4a3087bf004d3f69
plathrive <muhrasyid21048@gmail.com>
2026-03-06 09:16:26 +0700

    F: make grouping objects

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from logger import log_state
 from player import *
 
 def main():
+    # Initiating game program
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Grouping templates
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Setting FPS and player movements
     clock = pygame.time.Clock()
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
 
-
+    # Game Looping
     while True:
         log_state()
         for event in pygame.event.get():
@@ -24,8 +31,12 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        # Grouping objects for game loop 
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
