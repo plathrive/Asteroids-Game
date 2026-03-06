6902f14a047532cdacdcd7b2a79413560b2bf10f
plathrive <muhrasyid21048@gmail.com>
2026-03-06 08:49:01 +0700

    D: adding movement to player

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
